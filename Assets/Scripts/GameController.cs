ddc8b227a1d1c978dea5df545ed126cf37f26cd5
ArmatorX <17768182+ArmatorX@users.noreply.github.com>
2021-05-09 13:13:33 -0300

    Feat: First version of the game

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private List<Sprite> spritesLeft;
+    private List<GameObject> objectsShowing;
+    private List<ScriptFruta> fruitsShowing;
+    private int level;
+    private bool isFirstLevel;
+
+    private int errors;
+
+    public List<Sprite> SpriteList;
+    public GameObject ElementoFrutaPrefab;
+    public List<Sprite> SpritesLeft { get => spritesLeft; set => spritesLeft = value; }
+    public List<GameObject> ObjectsShowing { get => objectsShowing; set => objectsShowing = value; }
+    public List<ScriptFruta> FruitsShowing { get => fruitsShowing; set => fruitsShowing = value; }
+    public int Level { get => level; set => level = value; }
+    public bool IsFirstLevel { get => isFirstLevel; set => isFirstLevel = value; }
+    public int Errors { get => errors; set => errors = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Errors = 0;
+        Level = 3;
+        IsFirstLevel = true;
+        FruitsShowing = new List<ScriptFruta>();
+        ObjectsShowing = new List<GameObject>();
+
+        ShuffleSpriteList();
+        SpritesLeft = new List<Sprite>(SpriteList);
+
+        StartLevel();
+    }
+
+    void StartLevel()
+    {
+        if (Level > SpriteList.Count) 
+        {
+            Debug.Log("El tiempo total de juego fue " + Time.time + " s.");
+            Debug.Log("El paciente cometió " + Errors + " errores.");
+        } else
+        {
+            if (!IsFirstLevel)
+            {
+                ResetSpritesLeft();
+                DestroyAllFruits();
+            }
+            SpawnNFruits(Level);
+        }
+    }
+
+    void ResetSpritesLeft()
+    {
+        SpritesLeft = new List<Sprite>(SpriteList);
+    }
+
+    void DestroyAllFruits()
+    {
+        ObjectsShowing.ForEach((fruitObject) => Destroy(fruitObject));
+        ObjectsShowing.Clear();
+        FruitsShowing.Clear();
+    }
+
+    void SpawnNFruits(int n)
+    {
+        for (int i = 0; i < n; i ++)
+        {
+            SpawnFruit();
+        }
+
+        FruitsShowing[n-1].IsNewObject = true;
+        Debug.Log(FruitsShowing[n-1].spriteRenderer.sprite.name);
+        Debug.Log(FruitsShowing.Count);
+        Debug.Log(FruitsShowing[n-1].transform.position);
+    }
+
+    // Instancia un objeto nuevo
+    void SpawnFruit()
+    {
+        Vector3 position = GetUnoccupiedPosition();
+        GameObject fruta = Instantiate(ElementoFrutaPrefab, position, Quaternion.identity, GetComponent<Transform>());
+        ScriptFruta sFruta = fruta.GetComponent<ScriptFruta>();
+        sFruta.GameController = this;
+        FruitsShowing.Add(sFruta);
+        ObjectsShowing.Add(fruta);
+        sFruta.setSprite(SpritesLeft[0]);
+        sFruta.IsNewObject = false;
+        SpritesLeft.RemoveAt(0);
+    }
+
+    public void NextLevel()
+    {
+        if (IsFirstLevel)
+        {
+            IsFirstLevel = false;
+        }
+        IncrementLevel();
+        StartLevel();
+    }
+
+    public void ShuffleSpriteList()
+    {
+        List<Sprite> sprites = new List<Sprite>();
+        List<Sprite> spriteListAux = new List<Sprite>(SpriteList);
+
+        for (int i = 0; i < SpriteList.Count; i++)
+        {
+            int rand = Random.Range(0, spriteListAux.Count);
+            sprites.Add(spriteListAux[rand]);
+            spriteListAux.RemoveAt(rand);
+        }
+
+        SpriteList = sprites;
+    }
+
+    public void IncrementLevel()
+    {
+        Level++;
+    }
+
+    Vector3 GetUnoccupiedPosition()
+    {
+        Vector3 position;
+        do
+        {
+            position = new Vector3(Random.Range(-8, 8), Random.Range(-3.5f, 3.5f));
+        } while (IsPositionOccupied(position));
+
+        return position;
+    }
+
+    bool IsPositionOccupied(Vector2 position)
+    {
+        Vector2 p1 = position - new Vector2(1f, 1f);
+        Vector2 p2 = position + new Vector2(1f, 1f);
+        Collider2D collider = Physics2D.OverlapArea(p1, p2);
+
+        if (collider != null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void IncrementErrors()
+    {
+        Errors++;
+    }
+}
---
+++ Assets/Scripts/ScriptFruta.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ScriptFruta : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    private GameController gameController;
+    private bool isNewObject;
+
+    public GameController GameController { get => gameController; set => gameController = value; }
+    public bool IsNewObject { get => isNewObject; set => isNewObject = value; }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //ChangePositionToRandom();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setSprite(Sprite sprite)
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite;
+    }
+
+    //public void ChangePositionToRandom()
+    //{
+    //    Vector3 position = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f));
+
+    //    while (IsPositionOccupied(position))
+    //    {
+    //        position = new Vector3(Random.Range(-9, 9), Random.Range(-4.5f, 4.5f));
+    //    }
+
+    //    this.transform.position = position;
+    //}
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (IsNewObject)
+            {
+                GameController.NextLevel();
+            } else
+            {
+                GameController.IncrementErrors();
+            }
+        }
+    }
+
+    
+}
